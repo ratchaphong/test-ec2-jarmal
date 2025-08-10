4da178f2cf4a5e7f0f15af0a0e4ec0be111fae0d
ratchaphong <ratchaphongc1@gamil.com>
2025-08-10 13:46:42 +0700

    update port.

--- src/main.ts
+++ src/main.ts
@@ -14,8 +14,8 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document); // <--- http://localhost:3000/swagger
+  SwaggerModule.setup('swagger', app, document); // <--- http://localhost:4000/swagger
 
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
